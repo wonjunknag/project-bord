b1bdc12680e5f06a1a7367357552fa236c96710e
DESKTOP-P9E68JO\won <kwjcomein@gmail.com>
2025-09-06 22:43:24 +0900

    #1 - data rest 설정
    게시글, 댓글의 json api 를 자동을 restful 하게 만들게끔 설정

--- src/main/java/com/wonjuninfo/projectbord/repository/ArticleCommentRepository.java
+++ src/main/java/com/wonjuninfo/projectbord/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.wonjuninfo.projectbord.repository;
 
 import com.wonjuninfo.projectbord.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
