9fbd5125737f50e1a81c1675b7c8460a9911075a
DESKTOP-P9E68JO\won <kwjcomein@gmail.com>
2025-09-08 17:05:24 +0900

    #16 - 시큐리티 설정을 모든 요청에 인증이 열리도록 수정
    지금은 테스트 개발 단계고, 아직 인증 기능 구현 단계에
    이르지 않았으므로,
    모든 인증 설정을 오픈한다.
    그리고 폼 로그인을 활성화해서 로그인 뷰가 그려지게끔 했다.
    최신 스프링 부터 3.5에 시큐리티 설정을 함

---
+++ src/main/java/com/wonjuninfo/projectbord/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.wonjuninfo.projectbord.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .with(new FormLoginConfigurer<>(), form -> {})
+                .build();
+    }
+
+}
